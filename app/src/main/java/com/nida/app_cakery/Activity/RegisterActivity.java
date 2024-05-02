d0198936b78c4f7da531ac309855f0ee64495a62
Aksanur Konuk <88140679+konukaksanur@users.noreply.github.com>
2024-05-02 10:02:31 +0300

    Addes register functionality

--- app/src/main/java/com/nida/app_cakery/Activity/RegisterActivity.java
+++ app/src/main/java/com/nida/app_cakery/Activity/RegisterActivity.java
@@ -1,25 +1,37 @@
 package com.nida.app_cakery.Activity;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.nida.app_cakery.MainActivity;
 import com.nida.app_cakery.R;
 
 public class RegisterActivity extends AppCompatActivity {
     private String emailAddress, password;
     private String name, surname;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+        mAuth = FirebaseAuth.getInstance();
         registerActivity();
     }
 
@@ -54,10 +66,31 @@ public class RegisterActivity extends AppCompatActivity {
             return;
         }
         if (password.equals(passwordCheck)) {
-            //DATABASE'E REGISTER İÇİN GÖNDER!!!! BURDA EVET BURDA
-            //HERE AŞKOM
+            createUserWithEmailandPassword(emailAddress, password);
         } else {
             Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void createUserWithEmailandPassword(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+                    }
+                });
     }
 }
