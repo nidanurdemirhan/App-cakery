2f32c50fcd33f4b0d419cd55e51e8e7f6cca7310
elifekcby <149164383+elifekocabey@users.noreply.github.com>
2024-05-22 23:54:30 +0300

    ListActivity added

--- app/src/main/java/com/nida/app_cakery/Activity/ListActivity.java
+++ app/src/main/java/com/nida/app_cakery/Activity/ListActivity.java
@@ -1,6 +1,51 @@
 package com.nida.app_cakery.Activity;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nida.app_cakery.Adapters.RecipeAdapter;
+import com.nida.app_cakery.Domain.CakeryDomain;
+import com.nida.app_cakery.Listeners.FirebaseListener;
+import com.nida.app_cakery.Models.Recipe;
+import com.nida.app_cakery.R;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecipeAdapter recipeAdapter;
+    private ArrayList<Recipe> recipeList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        recyclerView = findViewById(R.id.recipeRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recipeList = new ArrayList<>();
+        recipeAdapter = new RecipeAdapter(this, recipeList);
+        recyclerView.setAdapter(recipeAdapter);
+
+        // Tarif verilerini al
+        fetchRecipes();
+    }
+
+    private void fetchRecipes() {
+        // Tarif verilerini CakeryDomain sınıfından al
+        CakeryDomain.getInstance().readRecipes(new FirebaseListener() {
+            @Override
+            public void onTaskCompleted() {
+                // Veri alındığında, RecyclerView'e ekle ve güncelle
+                recipeList.clear();
+                recipeList.addAll(CakeryDomain.getInstance().getRecipeList());
+                recipeAdapter.notifyDataSetChanged();
+            }
+        });
+    }
 }
