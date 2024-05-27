d02130b2fc09563dfaa29236f91914e1c23d9314
Duru Baştunalı <durubastunali@gmail.com>
2024-05-27 15:34:57 +0300

    craving test and craving activity

---
+++ app/src/main/java/com/nida/app_cakery/Activity/CravingActivity.java
@@ -0,0 +1,16 @@
+package com.nida.app_cakery.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.nida.app_cakery.R;
+
+public class CravingActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.craving_test_page);
+
+    }
+}
