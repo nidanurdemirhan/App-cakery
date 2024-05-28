e548a552c228effff32e75278c5ba9cbaf5b3bc1
Duru Baştunalı <durubastunali@gmail.com>
2024-05-28 15:27:50 +0300

    craving test result for cake

--- app/src/main/java/com/nida/app_cakery/Activity/CravingActivity.java
+++ app/src/main/java/com/nida/app_cakery/Activity/CravingActivity.java
@@ -1,6 +1,7 @@
 package com.nida.app_cakery.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -78,26 +79,26 @@ public class CravingActivity extends AppCompatActivity {
             public void onClick(View v) {
                 answer3.setVisibility(View.VISIBLE);
                 if (answer.equals("")) {
-                    answer += "0";
+                    answer = "0";
                     setQuestionAnswer(QAs, answer1, answer2, answer3, question, 1);
                 } else if (answer.equals("0")) {
-                    answer += "00";
+                    answer = "00";
                     setQuestionAnswer(QAs, answer1, answer2, answer3, question, 2);
                 } else if (answer.equals("1")) {
-                    answer += "10";
+                    answer = "10";
                     setQuestionAnswer(QAs, answer1, answer2, answer3, question, 6);
                 } else if (answer.equals("00")) {
-                    answer += "000"; //ID: 11
+                    answer = "000"; //ID: 11
                 } else if (answer.equals("01")) {
-                    answer += "010";
+                    answer = "010";
                 } else if (answer.equals("02")) {
-                    answer += "020"; //4
+                    answer = "020"; //4
                 } else if (answer.equals("10")) {
-                    answer += "100"; //ID = 12
+                    answer = "100"; //ID = 12
                 } else if (answer.equals("11")) {
-                    answer += "110";
+                    answer = "110";
                 } else if (answer.equals("12")) {
-                    answer += "120";
+                    answer = "120";
                 }
                 count++;
                 if (count == 3) {
@@ -113,26 +114,26 @@ public class CravingActivity extends AppCompatActivity {
                 answer3.setVisibility(View.VISIBLE);
 
                 if (answer.equals("")) {
-                    answer += "1";
+                    answer = "1";
                     setQuestionAnswer(QAs, answer1, answer2, answer3, question, 5);
                 } else if (answer.equals("0")) {
-                    answer += "01";
+                    answer = "01";
                     setQuestionAnswer(QAs, answer1, answer2, answer3, question, 3);
                 } else if (answer.equals("1")) {
-                    answer += "11";
+                    answer = "11";
                     setQuestionAnswer(QAs, answer1, answer2, answer3, question, 6);
                 } else if (answer.equals("00")) {
-                    answer += "001"; //ID = 1,2, 7
+                    answer = "001"; //ID = 1,2, 7
                 } else if (answer.equals("01")) {
-                    answer += "011"; //ID = 3,
+                    answer = "011"; //ID = 3,
                 } else if (answer.equals("02")) {
-                    answer += "021";
+                    answer = "021";
                 } else if (answer.equals("10")) {
-                    answer += "101"; //ID = 12
+                    answer = "101"; //ID = 12
                 } else if (answer.equals("11")) {
-                    answer += "111";
+                    answer = "111";
                 } else if (answer.equals("12")) {
-                    answer += "121";
+                    answer = "121";
                 }
                 count++;
                 if (count == 3) {
@@ -146,23 +147,23 @@ public class CravingActivity extends AppCompatActivity {
             public void onClick(View v) {
                 answer3.setVisibility(View.VISIBLE);
                 if (answer.equals("0")) {
-                    answer += "02";
+                    answer = "02";
                     setQuestionAnswer(QAs, answer1, answer2, answer3, question, 4);
                 } else if (answer.equals("1")) {
-                    answer += "12";
+                    answer = "12";
                     setQuestionAnswer(QAs, answer1, answer2, answer3, question, 7);
                 } else if (answer.equals("00")) {
-                    answer += "002"; //ID = 8,
+                    answer = "002"; //ID = 8,
                 } else if (answer.equals("01")) {
-                    answer += "012";
+                    answer = "012";
                 } else if (answer.equals("02")) {
-                    answer += "022"; //ID = 6
+                    answer = "022"; //ID = 6
                 } else if (answer.equals("10")) {
-                    answer += "102";
+                    answer = "102";
                 } else if (answer.equals("11")) {
-                    answer += "112";
+                    answer = "112";
                 } else if (answer.equals("12")) {
-                    answer += "122"; //ID = 9
+                    answer = "122"; //ID = 9
                 }
                 count++;
                 if (count == 3) {
@@ -178,6 +179,7 @@ public class CravingActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerViewTEST);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        /*
         ImageButton btnCart = findViewById(R.id.btnCart);
         ImageButton btnFav = findViewById(R.id.btnFavorites);
         ImageButton btnHome = findViewById(R.id.btnHome);
@@ -191,7 +193,7 @@ public class CravingActivity extends AppCompatActivity {
         btnHome.setOnClickListener(v -> startActivity(new Intent(CravingActivity.this, HomeActivity.class)));
         btnAdd.setOnClickListener(v -> startActivity(new Intent(CravingActivity.this, AddActivity.class)));
         btnList.setOnClickListener(v -> startActivity(new Intent(CravingActivity.this, ListActivity.class)));
-
+*/
 
         CakeryDomain.getInstance().readRecipes(new FirebaseListener() {
             @Override
@@ -205,8 +207,9 @@ public class CravingActivity extends AppCompatActivity {
 
     private ArrayList<Recipe> getRecipes(ArrayList<Recipe> allRecipes) {
         ArrayList<Recipe> resultRecipes = new ArrayList<>();
+        Log.d("ANSWER", answer);
         for (int i = 0; i < allRecipes.size(); i++) {
-            if (answer.equals("000") && allRecipes.get(i).getRecipeID().equals("1")) { //CUPCAKE
+            if (answer.equals("000")) { //CUPCAKE
 
             } else if (answer.equals("010")) { //KURU YEMİŞLİ KURABİYE
 
@@ -219,9 +222,10 @@ public class CravingActivity extends AppCompatActivity {
             } else if (answer.equals("120")) { //KRAKER
 
             } else if (answer.equals("001") && (allRecipes.get(i).getRecipeID().equals("1") ||
-                    allRecipes.get(i).getRecipeID().equals("2"))) { //PASTA KEK
+                    allRecipes.get(i).getRecipeID().equals("2"))) { //PASTA KEK = 1, 2
+                Log.d("GİRDİ", "GİRDİ");
                 resultRecipes.add(allRecipes.get(i));
-            } else if (answer.equals("011") && allRecipes.get(i).getRecipeID().equals("3")) { //ÇİKOLATALI KURABİYE
+            } else if (answer.equals("011")) { //ÇİKOLATALI KURABİYE
 
             } else if (answer.equals("021")) { //ŞERBETLİ TATLI
 
@@ -245,7 +249,7 @@ public class CravingActivity extends AppCompatActivity {
 
             }
         }
-        return allRecipes;
+        return resultRecipes;
     }
 
     private void setQuestionAnswer(String[][] arrayQA, Button ans1, Button ans2, Button ans3, TextView q, int i) {
